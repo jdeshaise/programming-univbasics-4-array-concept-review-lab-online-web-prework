860c09b1e5b1e6dfb6fd12b73333e4a8b6138473
Joseph Miller <jdeshaiseworldtv@gmail.com>
2019-09-20 02:24:38 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,40 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+
+  array [1,5,7,3,9]
+
+  counter = 0
+  b. = [1]
+  c. = [2]
+  d. = [3]
+  e. = [4]
+
+  value_to_find?
+
+  minvalue.b =[5]
+  maxvalue.e =[9]
+
+
 end
 
 def find_max_value(array)
-  # Add your solution here
+array =  [1,2,1,3,4,3,5,4,3,2,1]
+
+  a. = [0]
+  b. = [1]
+  c. = [2]
+  d. = [3]
+  e. = [4]
+  f. = [5]
+
+ array.f [4] #=5
+
+
 end
 
 def find_min_value(array)
-  # Add your solution here
+
+find_min_value([10,5,3,7,19,1,3,10])
+
+find_min_value.array=[1]
+
 end
